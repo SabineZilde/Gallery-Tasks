e4c40b3e18d85dac844183c0f14b2a81befe89c6
SabineZilde <sabine.zilde@protonmail.ch>
2021-04-14 10:08:45 +0300

    Improved version

--- js/task2.js
+++ js/task2.js
@@ -1,61 +1,8 @@
-$(document).ready(function() {
-    $('#img2-large').hide();
-    $('#img3-large').hide();
-    $('#img4-large').hide();
-    $('#img5-large').hide();
-    $('#img6-large').hide();
-});
-
-$('#img1').hover(function() {
-    $('#img1-large').show();
-    $('#img2-large').hide();
-    $('#img3-large').hide();
-    $('#img4-large').hide();
-    $('#img5-large').hide();
-    $('#img6-large').hide();
-});
-
-$('#img2').hover(function() {
-    $('#img1-large').hide();
-    $('#img2-large').show();
-    $('#img3-large').hide();
-    $('#img4-large').hide();
-    $('#img5-large').hide();
-    $('#img6-large').hide();
-});
-
-$('#img3').hover(function() {
-    $('#img1-large').hide();
-    $('#img2-large').hide();
-    $('#img3-large').show();
-    $('#img4-large').hide();
-    $('#img5-large').hide();
-    $('#img6-large').hide();
-});
-
-$('#img4').hover(function() {
-    $('#img1-large').hide();
-    $('#img2-large').hide();
-    $('#img3-large').hide();
-    $('#img4-large').show();
-    $('#img5-large').hide();
-    $('#img6-large').hide();
-});
-
-$('#img5').hover(function() {
-    $('#img1-large').hide();
-    $('#img2-large').hide();
-    $('#img3-large').hide();
-    $('#img4-large').hide();
-    $('#img5-large').show();
-    $('#img6-large').hide();
-});
-
-$('#img6').hover(function() {
-    $('#img1-large').hide();
-    $('#img2-large').hide();
-    $('#img3-large').hide();
-    $('#img4-large').hide();
-    $('#img5-large').hide();
-    $('#img6-large').show();
+$('img').hover(function() {
+    const link = $(this).attr('src');
+    const altText = $(this).attr('alt');
+    $('#img-large').attr({
+        'src': link, 
+        'alt': altText
+    });
 });
